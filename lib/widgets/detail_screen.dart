4862c14d81acc1937980234d29555b19e0b95643
yoonyoul <yoonyoul@linecorp.com>
2023-11-22 09:36:48 +0900

    episode and uri-launcher

--- lib/widgets/detail_screen.dart
+++ lib/widgets/detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:toonflix/models/webtoon_details_model.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetailScreen extends StatefulWidget {
   final WebtoonModel webtoon;
@@ -41,48 +42,76 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50,
+            vertical: 40,
+          ),
+          child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Hero(
-                  tag: widget.webtoon.id,
-                  child: Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 15,
-                          offset: const Offset(10, 10),
-                          color: Colors.black.withOpacity(0.3),
-                        )
-                      ],
-                    ),
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Image.network(
-                      widget.webtoon.thumb,
-                      headers: const {
-                        "User-Agent":
-                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                      },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.webtoon.id,
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.3),
+                          )
+                        ],
+                      ),
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Image.network(
+                        widget.webtoon.thumb,
+                        headers: const {
+                          "User-Agent":
+                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                        },
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
+              const SizedBox(
+                height: 25,
+              ),
+              buildWebToonDetails(),
+              const SizedBox(
+                height: 10,
+              ),
+              buildEpisodes(widget.webtoon.id)
             ],
           ),
-          const SizedBox(
-            height: 25,
-          ),
-          buildWebToonDetails()
-        ],
+        ),
       ),
     );
+  }
+
+  FutureBuilder<List<WebtoonEpisodeModel>> buildEpisodes(String webtoonId) {
+    return FutureBuilder(
+        future: webtoonEpisodes,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                for (var episode in snapshot.data!)
+                  Episode(
+                    episode: episode,
+                    webtoonId: webtoonId,
+                  )
+              ],
+            );
+          } else {
+            return const Text('No episodes');
+          }
+        });
   }
 
   FutureBuilder<WebtoonDetailsModel> buildWebToonDetails() {
@@ -90,31 +119,88 @@ class _DetailScreenState extends State<DetailScreen> {
         future: webtoonDetails,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: Column(
-                children: [
-                  Text(
-                    snapshot.data!.about,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
+            return Column(
+              children: [
+                Text(
+                  snapshot.data!.about,
+                  style: const TextStyle(
+                    fontSize: 16,
                   ),
-                  const SizedBox(
-                    height: 10,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '${snapshot.data!.genre}/${snapshot.data!.age}',
+                  style: const TextStyle(
+                    fontSize: 16,
                   ),
-                  Text(
-                    '${snapshot.data!.genre}/${snapshot.data!.age}',
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             );
           } else {
             return const Text('...');
           }
         });
+  }
+}
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+
+  final WebtoonEpisodeModel episode;
+  final String webtoonId;
+
+  void onButtonTap() async {
+    final url = Uri.parse(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
+    await launchUrl(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 15,
+              offset: const Offset(10, 10),
+              color: Colors.black.withOpacity(0.3),
+            )
+          ],
+          color: Colors.green.shade400,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
